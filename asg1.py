6ab0add0fd272467a3825ef125a5e56ad622918d
asiChakib <abdelmounaim.chakib.salouani@gmail.com>
2025-02-20 17:12:18 +0100

    code done, visualizer to verify it

--- asg1.py
+++ asg1.py
@@ -20,38 +20,48 @@ def regular_prism(origin, height, radius, faces):
         #========================
         it = list(item)
         it[-1] += height
-        vertices2.append(tuple(it))
+        vertex = tuple(it)
+        vertices2.append(vertex)
+        #========================
+        edges.append((item, vertex))
         
+        
+    for i in range(faces-1):
+        edges.append((vertices[i], vertices[i+1]))
+        edges.append((vertices2[i], vertices2[i+1]))
+
+    edges.append((vertices[-1], vertices[0]))
+    edges.append((vertices2[-1], vertices2[0]))
+
     vertices.extend(vertices2)
     del vertices2
-        
-    return edges
-
-o = (0,0,0)
-vert = regular_prism(o, 3, 1, 4)
-
-
-
-def scatter_prism(vert):
-    ver = np.array(vert)
-    x, y, z = ver[:, 0], ver[:, 1], ver[:, 2]
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    ax.scatter(x, y, z, c='b', marker='o')  # 'c' for color, 'o' for marker style
-
-    ax.plot([0, max(x)], [0, 0], [0, 0], color='r', linewidth=2, label="X-axis")  # X-axis in red
-    ax.plot([0, 0], [0, max(y)], [0, 0], color='g', linewidth=2, label="Y-axis")  # Y-axis in green
-    ax.plot([0, 0], [0, 0], [0, max(z)], color='b', linewidth=2, label="Z-axis")  # Z-axis in blue
-
-    # Labels
-    ax.set_xlabel("X Axis")
-    ax.set_ylabel("Y Axis")
-    ax.set_zlabel("Z Axis")
-    ax.set_title("3D Scatter Plot")
-
-    plt.show()
     
-#scatter_prism(vert)
+    return vertices, edges
 
-print(vert)
+# o = (0,0,0)
+# vert = regular_prism(o, 3, 1, 4)
+
+
+
+# def scatter_prism(vert):
+#     ver = np.array(vert)
+#     x, y, z = ver[:, 0], ver[:, 1], ver[:, 2]
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
+
+#     ax.scatter(x, y, z, c='b', marker='o')  # 'c' for color, 'o' for marker style
+
+#     ax.plot([0, max(x)], [0, 0], [0, 0], color='r', linewidth=2, label="X-axis")  # X-axis in red
+#     ax.plot([0, 0], [0, max(y)], [0, 0], color='g', linewidth=2, label="Y-axis")  # Y-axis in green
+#     ax.plot([0, 0], [0, 0], [0, max(z)], color='b', linewidth=2, label="Z-axis")  # Z-axis in blue
+
+#     # Labels
+#     ax.set_xlabel("X Axis")
+#     ax.set_ylabel("Y Axis")
+#     ax.set_zlabel("Z Axis")
+#     ax.set_title("3D Scatter Plot")
+
+#     plt.show()
+    
+# scatter_prism(vert)
+
